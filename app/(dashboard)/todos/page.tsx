adf4e47581e1cef920df68710fffe73d49c04078
opagani <paganio@yahoo.com>
2023-07-31 14:57:35 -0700

    Modified next.config.js file to ignore errors during build

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -4,13 +4,12 @@ import db from '@/utils/db'
 const getData = async () => {
   // await new Promise((resolve, reject) => setTimeout(() => reject(), 2000))
   const todos = await db.todo.findMany({})
-  // console.log(todos)
+  console.log(todos)
   return todos
 }
 
 const TodosPage = async () => {
   const todos = await getData()
-  console.log('todos', todos)
   return (
     <div>
       <TodoList todos={todos} />
--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -4,8 +4,7 @@ const TodoList = ({ todos }: any) => {
   return (
     <div>
       {todos.map((todo: any) => (
-        // <Todo todo={todo} key={todo.id} />
-        <div key={todo.id}>hello</div>
+        <Todo todo={todo} key={todo.id} />
       ))}
     </div>
   )
